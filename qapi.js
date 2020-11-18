f2ceacd5b2a38c59234fa7a79bce3e11fc4d45fd
karandhanwani16 <54567222+karandhanwani16@users.noreply.github.com>
2020-11-19 03:39:23 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fb59a95b83a2f001f2e108c",
+				"5fb59a95b83a2f001f2e108e"
+			]
+		},
+		"5fb59a95b83a2f001f2e108c": {
+			"id": "5fb59a95b83a2f001f2e108c",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fb59a95b83a2f001f2e108e": {
+			"id": "5fb59a95b83a2f001f2e108e",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
